4930501ed665eabfd781967f3d8da88f02c077ca
bmk2010 <profiy70@gmail.com>
2024-11-11 16:24:51 +0500

    fix: error scrollbar

--- src/components/RenderHomes.tsx
+++ src/components/RenderHomes.tsx
@@ -24,33 +24,35 @@ const RenderHomes: React.FC = () => {
 
   return (
     <div className="flex gap-[52px]">
-      {homes.slice(7).map((home: homeType, i: number) => (
-        <div
-          className="flex flex-col gap-8 max-w-[300px] bg-[white] rounded-xl pb-3"
-          key={i}
-        >
-          <img className="rounded-t-xl" src={home.img} alt="" />
-          <div className="mx-[25px]">
-            <p className="text-[22px] text-[#161A1D]">{home.title}</p>
-            <p className="text-[#6A9B0C]">$ {home.price}</p>
-          </div>
-          <div className="text-[#999999] mx-[25px] flex justify-between">
-            {home.when}
-            <img width={25} src={Like} alt="" />
-          </div>
-          <div className="mx-[25px] flex justify-between">
-            <div className="flex gap-8">
-              <img src={Rooms} alt="" />
-              <img src={Area} alt="" />
-              <img src={Remont} alt="" />
+      {homes
+        .slice(homes.length - homes.length + 3)
+        .map((home: homeType, i: number) => (
+          <div
+            className="flex flex-col gap-8 max-w-[300px] bg-[white] rounded-xl pb-3"
+            key={i}
+          >
+            <img className="rounded-t-xl" src={home.img} alt="" />
+            <div className="mx-[25px]">
+              <p className="text-[22px] text-[#161A1D]">{home.title}</p>
+              <p className="text-[#6A9B0C]">$ {home.price}</p>
             </div>
-            <div className="flex gap-2">
-              <img src={Eye} alt="" />
-              {home.saws}
+            <div className="text-[#999999] mx-[25px] flex justify-between">
+              {home.when}
+              <img width={25} src={Like} alt="" />
+            </div>
+            <div className="mx-[25px] flex justify-between">
+              <div className="flex gap-8">
+                <img src={Rooms} alt="" />
+                <img src={Area} alt="" />
+                <img src={Remont} alt="" />
+              </div>
+              <div className="flex gap-2">
+                <img src={Eye} alt="" />
+                {home.saws}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
